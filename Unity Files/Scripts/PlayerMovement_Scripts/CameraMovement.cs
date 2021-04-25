c0db65d89f47b749792e38469a3c5c7d281a2872
Joao Milone <joaocmil@hotmail.com>
2021-04-25 12:21:36 -0300

    Tweaked code

--- Unity Files/Scripts/PlayerMovement_Scripts/CameraMovement.cs
+++ Unity Files/Scripts/PlayerMovement_Scripts/CameraMovement.cs
@@ -16,6 +16,7 @@ public class CameraMovement : MonoBehaviour
 
     private float verticalRotation = 0;
 
+
     private void Start()
     {
         MouseLocker.LockMouse();
@@ -34,7 +35,26 @@ public class CameraMovement : MonoBehaviour
             MouseLocker.FreeMouse();
 
         if (Input.GetKeyDown(KeyCode.Mouse0))
-            MouseLocker.LockMouse();  
+            MouseLocker.LockMouse();
+    }
+
+    void CameraVerticalMovement()
+    {
+        if (cameraTranform == null)
+            return;
+
+        verticalRotation -= Y_ValueWithSens();
+        verticalRotation = RotationClamped(verticalRotation);
+
+        cameraTranform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+    }
+
+    void CameraHorizontalMovement()
+    {
+        if (playerTransform == null)
+            return;
+
+        playerTransform.Rotate(Vector3.up * X_ValueWithSens());
     }
 
     public void OverrideLookAt(Transform targetToLook)
@@ -52,22 +72,8 @@ public class CameraMovement : MonoBehaviour
         return PlayerInputs.Camera_Y_Movement() * Time.deltaTime * mouseSensY;
     }
 
-    void CameraHorizontalMovement() 
+    float RotationClamped(float refRotation) 
     {
-        if (playerTransform == null)
-            return;
-
-        playerTransform.Rotate(Vector3.up * X_ValueWithSens());
-    }
-
-    void CameraVerticalMovement()
-    {
-        if (cameraTranform == null)
-            return;
-
-        verticalRotation -= Y_ValueWithSens();
-        verticalRotation = Mathf.Clamp(verticalRotation, minClampVertical, maxClampHorizontal);
-
-        cameraTranform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f);
+        return Mathf.Clamp(refRotation, minClampVertical, maxClampHorizontal);
     }
 }
